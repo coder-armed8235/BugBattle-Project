06876732b5b15047338ffd3f8047c9736ce1beaf
Amit Kumar <www.yadavanshiamit@gmail.com>
2026-09-16 08:08:15 +0530

    redis fixing

--- Backend/src/config/redis.js
+++ Backend/src/config/redis.js
@@ -4,8 +4,8 @@ const redisClient = createClient({
     username: 'default',
     password: process.env.REDIS_PASS,
     socket: {
-        host: 'redis-11165.crce276.ap-south-1-3.ec2.cloud.redislabs.com',
-        port: 11165
+        host: 'redis-16301.crce217.ap-south-1-1.ec2.cloud.redislabs.com',
+        port: 16301
     }
 });
 
@@ -13,23 +13,4 @@ const redisClient = createClient({
 module.exports=redisClient;
 
 
-
-// import { createClient } from 'redis';
-
-// const client = createClient({
-//     username: 'default',
-//     password: 'DWtZaywbifUZEfpPiZ6qLVs4cJALoArH',
-//     socket: {
-//         host: 'redis-18487.crce206.ap-south-1-1.ec2.cloud.redislabs.com',
-//         port: 18487
-//     }
-// });
-
-// client.on('error', err => console.log('Redis Client Error', err));
-
-// await client.connect();
-
-// await client.set('foo', 'bar');
-// const result = await client.get('foo');
-// console.log(result)  // >>> bar
 
